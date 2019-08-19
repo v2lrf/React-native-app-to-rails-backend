81cca1500c347b0083aaecb248203e3a9d2642f5
v baxter <vincent.viricel@gmail.com>
2019-08-19 20:03:12 +0200

    some update vbaxt

--- config/api/credentials.js
+++ config/api/credentials.js
@@ -1,9 +1,9 @@
 export const Url = {
   headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
   },
-  login: 'http://192.168.1.24:3000/api/v1/auth/sign_in',
-  validate_token: 'http://192.168.1.24:3000/api/v1/auth/validate_token',
-  appointments: 'http://192.168.1.24:3000/api/v1/appointments/',
-}
+  login: 'http://http://192.168.1.43:3000/api/v1/auth/sign_in',
+  validate_token: 'http://http://192.168.1.43:3000/api/v1/auth/validate_token',
+  appointments: 'http://http://192.168.1.43:3000/api/v1/appointments/'
+};
